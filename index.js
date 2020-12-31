34b51cf114f733a9bd16b9a801ef1671e5625300
sagacious-shell-7968 <tommysytc.tc@gmail.com>
2020-12-31 06:16:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your Circle class here
+class Circle {
+  constructor(radius){
+    this.radius=radius;
+  }
+  get diameter(){
+    return this.radius*2;
+  }
+  get circumference(){
+    return 2*Math.pi*this.radius;
+  }
+  get area(){
+    return Math.pi*this.radius*this.radius;
+  }
+   set diameter(radius){
+    this.diameter=radius*2;
+  }
+  set circumference(){
+    this.circumference=2*Math.pi*this.radius;
+  }
+  set area(){
+    this.area=Math.pi*this.radius*this.radius;
+  }
+}
