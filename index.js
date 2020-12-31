68bd019e36db6355fd95353efd3cabf3de8054a0
sagacious-shell-7968 <tommysytc.tc@gmail.com>
2020-12-31 06:18:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ class Circle {
     return this.radius*2;
   }
   get circumference(){
-    return 2*Math.pi*this.radius;
+    return 2*Math.PI*this.radius;
   }
   get area(){
-    return Math.pi*this.radius*this.radius;
+    return Math.PI*this.radius*this.radius;
   }
    set diameter(radius){
     this.diameter=radius*2;
   }
   set circumference(radius){
-    this.circumference=2*Math.pi*radius;
+    this.circumference=2*Math.PI*radius;
   }
   set area(radius){
-    this.area=Math.pi*radius*radius;
+    this.area=Math.PI*radius*radius;
   }
 }
